b97164b09f47236380bb9fbd6d2a46810614756e
gustavogarcia-dev <gggl31128@gmail.com>
2024-05-20 12:01:16 -0400

    resolviendo problemas con la imagen

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,5 @@
 import TitleWriter from '../components/titleWriter';
-import homePage from '../images/home-page.jpeg'
+import homePage from '../images/home-page.webp.webp'
 import CV from '../Documents/Gustavo Garcia frontend.pdf'
 export default function Home() {
     return (
@@ -31,7 +31,7 @@ export default function Home() {
 
                     <a href={CV} target='_blank' title='Gustavo-garcia-Frontend-CV' className="w-100 socialContainer containerFive ">
                         <svg className="socialSvg " xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M15 11h7v2h-7zm1 4h6v2h-6zm-2-8h8v2h-8zM4 19h10v-1c0-2.757-2.243-5-5-5H7c-2.757 0-5 2.243-5 5v1h2zm4-7c1.995 0 3.5-1.505 3.5-3.5S9.995 5 8 5 4.5 6.505 4.5 8.5 6.005 12 8 12z"></path></svg>
-                        Descargar CV
+                        <span className='text-cv'>Descargar CV</span>
                     </a>
                 </div>             
 
@@ -39,11 +39,16 @@ export default function Home() {
 
             </div>
             
-            <div className=" d-flex justify-content-center animate__animated animate__fadeIn">
-                <div className="rounded-circle overflow-hidden border border-dark shadow"  style={{ width: "350px", height: "350px"}}>
-                    <img className="img-fluid " src={homePage} alt="Perfil" style={{ width: "100%", height: "100%", }} />
-                </div>
+            <div className="d-flex justify-content-center animate__animated animate__fadeIn">
+            <div className="rounded-circle overflow-hidden border border-dark shadow" style={{ width: "350px", height: "350px" }}>
+                <picture>
+                    <source srcSet={homePage} type="image/webp" />
+                    {/* <source srcSet={homePage} type="image/jpeg" /> */}
+                    <img className="img-fluid" src={homePage} alt="Perfil" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
+                </picture>
             </div>
+        </div>
+
 
 
 
